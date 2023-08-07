32ae51e6137e40aa1c04b7efcfe73faefa4d9a80
Bringas-tunnel <131277468+Bringas-tunnel@users.noreply.github.com>
2023-08-07 11:54:52 +0700

    Update menu-domain.sh

--- menu/menu-domain.sh
+++ menu/menu-domain.sh
@@ -9,7 +9,7 @@ echo -e ""
 echo -e "     ${BICyan}[${BIWhite}01${BICyan}] Change Domain"
 echo -e "     ${BICyan}[${BIWhite}02${BICyan}] Domain Cert"
 echo -e "     ${BICyan}[${BIWhite}03${BICyan}] Back To Menu"
-echo -e " ${BICyan}└─────────────────────────────────────────────────────┘${NC}"
+echo -e "${BICyan} └─────────────────────────────────────────────────────┘${NC}"
 echo -e ""
 read -p " Select menu : " xyz
 echo -e ""
