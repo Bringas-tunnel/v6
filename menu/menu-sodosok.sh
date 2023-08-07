dfae749085d4d1278b85556f3ed7e2fa0e73fb80
Bringas-tunnel <131277468+Bringas-tunnel@users.noreply.github.com>
2023-08-07 12:04:57 +0700

    Update menu-sodosok.sh

--- menu/menu-sodosok.sh
+++ menu/menu-sodosok.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# color validation
+BICyan='\033[1;36m'
+BIWhite='\033[1;37m'
+NC='\033[0m'
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
@@ -14,9 +18,9 @@ clear
 echo -e "${BICyan} ┌─────────────────────────────────────────────────────┐${NC}"
 echo -e "${BICyan} │  ${BICyan}[${BIWhite}01${BICyan}] Create Shadowsock   ""${BICyan}[${BIWhite}04${BICyan}] Check Shadowsock"
 echo -e "${BICyan} │  ${BICyan}[${BIWhite}02${BICyan}] Delete Shadowsock   ""${BICyan}[${BIWhite}05${BICyan}] Triall Shadowsock"
-echo -e "${BICyan} │  ${BICyan}[${BIWhite}03${BICyan}] Renew Shadowsock    ""${BICyan}[${BIWhite}06${BICyan}] Back to Menu'
+echo -e "${BICyan} │  ${BICyan}[${BIWhite}03${BICyan}] Renew Shadowsock    ""${BICyan}[${BIWhite}06${BICyan}] Back to Menu"
 echo -e "${BICyan} └─────────────────────────────────────────────────────┘${NC}"
-echo ""
+echo - ""
 read -p "select Menu --- >>>  " opt
 case $opt in
 1) clear ; addss ; exit ;;
